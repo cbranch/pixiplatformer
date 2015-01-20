10d6e8c8767457be9f9b6c98bf2dacda8d79a567
Chris Branch <chris@chrisbranch.co.uk>
2015-01-20 20:53:18 +0000

    Create stubs for other levels

--- js/leveldata.js
+++ js/leveldata.js
@@ -1,4 +1,4 @@
-define(['text!levels/level1.json'],
-  function(level1) {
-    return [JSON.parse(level1)];
+define(['text!levels/level1.json','text!levels/level2.json','text!levels/level3.json'],
+  function(level1, level2, level3) {
+    return [JSON.parse(level1), JSON.parse(level2), JSON.parse(level3)];
   });
